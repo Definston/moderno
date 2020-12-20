9e721391a72aa6e6500e9c46079c0d54a9cf5047
Definston <definston@gmail.com>
2020-12-21 02:29:25 +0600

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(function(){
 
 
-    
+
     var mixer = mixitup ('.products__inner-box');
+
+    $('.product-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });
 
 });
